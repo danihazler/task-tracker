4f560ffd33469b65fe8b384b85ec8bc181cce34a
Dani <danihazler@gmail.com>
2021-10-19 23:47:55 +0100

    Add Header component

---
+++ src/components/button/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <button
+        class="btn"
+        :style="{background: color}"
+        @click="onClick()" 
+    >
+        {{ label }}
+    </button>
+</template>
+
+<script>
+    export default {
+        name: "Button",
+        props: {
+            label: String,
+            color: String,
+        },
+        methods: {
+            onClick() {
+                console.log("click");
+            }
+        }
+    }
+</script>
